02cc2194632643104427b9b13c93f8e1635b2587
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-03-23 03:14:28 -0300

    Se agregó func: cuadrado de un numero

--- tp2_2.cpp
+++ tp2_2.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+//Declaracion de funciones
+int cuadradoNumero(int num);
+void cuadradoNumeroVoid(int num);
+
+//Programa principal
 int main(){    
 
+    int num;
+    printf("Ingresar un numero: ");
+    scanf("%d",&num);
+    printf("El cuadrado de %d es: %d ", num, cuadradoNumero(num));
+
+    cuadradoNumeroVoid(num);
 
     return 0;
 }
+
+//Definicion de funciones
+int cuadradoNumero(int num){
+    int cuadrado;
+    cuadrado= num*num;
+    return cuadrado;
+}
+
+void cuadradoNumeroVoid(int num){
+    int cuadrado;
+    cuadrado= num*num;
+    printf("\nEl cuadrado de %d es: %d ", num, cuadrado);
+}
+
