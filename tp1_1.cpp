66bc8f2c9e5b44a481b8944bee31d6648ec0a165
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-03-22 21:14:43 -0300

    Punto 3e: punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(){
+int main(){    
+    printf("\n Hola mundo!");
+
+    //Declaración de variables y punteros
+    int edad=20;
+    int *puntero;
     
-    printf("Hola mundo");
+    //Puntero apunta a la variable edad
+    puntero = &edad;
+
+    printf("\n Contenido de puntero: %d", *puntero);
+    printf("\n La direcci%cn de memoria almacenada por el puntero es: %p",162, puntero);
+    printf("\n La direcci%cn de la variable edad es: %p", 162, &edad);
+    printf("\n La direcci%cn de memoria del puntero es: %p", 162, &puntero);
+    printf("\n El tama%co de memoria es: %lu", 164, sizeof(edad));
+
     return 0;
 }
